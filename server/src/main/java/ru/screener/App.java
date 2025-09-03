e100108054c81d99467c8f70a20bc6d845d53070
Nikita Semenov <nikita.semenov@megafon.ru>
2025-09-03 03:53:23 +0300

    Add reactive

--- server/src/main/java/ru/screener/App.java
+++ server/src/main/java/ru/screener/App.java
@@ -3,13 +3,9 @@ package ru.screener;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "ru.screener.strategy.integrations")
-@EnableRetry
 @EnableKafka
 public class App {
     public static void main(String[] args) {
--- server/src/main/java/ru/screener/service/strategy/rsi/RsiProcessor.java
+++ server/src/main/java/ru/screener/service/strategy/rsi/RsiProcessor.java
@@ -3,6 +3,8 @@ package ru.screener.service.strategy.rsi;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.screener.model.kafka.bybit.notification.NotificationEvent;
 import ru.screener.model.kafka.bybit.settings.RsiSettingsEvent;
 import ru.screener.model.kafka.bybit.strategies.RsiEvent;
@@ -11,6 +13,7 @@ import ru.screener.model.strategies.Strategy;
 import ru.screener.producer.notification.NotificationPublisher;
 import ru.screener.service.strategy.StrategyProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -29,13 +32,14 @@ public class RsiProcessor implements StrategyProcessor<RsiEvent> {
         userStrategies.forEach((userId, settings) -> {
             for (RsiSettingsEvent setting : settings) {
                 if (!rsiEvent.getTimeframe().equals(setting.getShortTimeFrame())
-                && !rsiEvent.getTimeframe().equals(setting.getLongTimeFrame())) {
+                        && !rsiEvent.getTimeframe().equals(setting.getLongTimeFrame())) {
                     continue;
                 }
 
                 if (rsiEvent.getTimeframe().equals(setting.getShortTimeFrame())
                         && rsiEvent.getRsi().compareTo(setting.getShortRsi()) > 0) {
-                    Integer strategyCount = strategyCounts.getOrDefault(setting.getTelegramId(), 0);
+
+                    int strategyCount = strategyCounts.getOrDefault(setting.getTelegramId(), 0);
 
                     NotificationEvent notification = new NotificationEvent()
                             .setTelegramId(setting.getTelegramId())
@@ -43,12 +47,27 @@ public class RsiProcessor implements StrategyProcessor<RsiEvent> {
                             .setSymbol(rsiEvent.getSymbol())
                             .setTimeframe(rsiEvent.getTimeframe())
                             .setDirection(Direction.DOWN)
+                            .setRsi(rsiEvent.getRsi())
                             .setSignalNumber(strategyCount);
-                    notificationPublisher.sendMessage(notification);
 
-                    strategyCounts.computeIfAbsent(setting.getTelegramId(), k -> 0);
+                    notificationPublisher.sendMessage(notification);
+                    strategyCounts.put(setting.getTelegramId(), strategyCount + 1);
                 }
             }
         });
+    }
+
+    public void updateUserStrategy(RsiSettingsEvent setting) {
+        userStrategies.compute(setting.getTelegramId(), (user, list) -> {
+            List<RsiSettingsEvent> updated = (list != null) ? new ArrayList<>(list) : new ArrayList<>();
+            updated.add(setting);
+            return updated;
+        });
+    }
+
+    public Mono<Void> loadInitialSettings(Flux<RsiSettingsEvent> settingsFlux) {
+        return settingsFlux
+                .doOnNext(this::updateUserStrategy)
+                .then();
     }
 }
