c57637c69a7572e605140e2a28d880c85024d961
Nikita Semenov <nikita.semenov@megafon.ru>
2025-09-13 23:06:56 +0300

    Update RsiEvent.java

--- server/src/main/java/ru/screener/model/kafka/bybit/strategies/RsiEvent.java
+++ server/src/main/java/ru/screener/model/kafka/bybit/strategies/RsiEvent.java
@@ -1,12 +1,16 @@
 package ru.screener.model.kafka.bybit.strategies;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Accessors(chain = true)
 public class RsiEvent {
 
