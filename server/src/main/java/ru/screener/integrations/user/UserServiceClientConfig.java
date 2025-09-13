3c586fcd5b06a02e754dc59620822d6d759997af
Nikita Semenov <nikita.semenov@megafon.ru>
2025-09-13 05:02:34 +0300

    Apply changes

--- server/src/main/java/ru/screener/integrations/user/UserServiceClientConfig.java
+++ server/src/main/java/ru/screener/integrations/user/UserServiceClientConfig.java
@@ -3,21 +3,16 @@ package ru.screener.integrations.user;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class UserServiceClientConfig {
 
     @Bean
-    public WebClient userServiceWebClient(
-            WebClient.Builder builder,
-            @Value("${services.user-service.base-url}") String baseUrl
-    ) {
+    public WebClient userServiceWebClient(WebClient.Builder builder,
+                                          @Value("${services.user-service.base-url}") String baseUrl) {
         return builder
                 .baseUrl(baseUrl)
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_NDJSON_VALUE)
                 .build();
     }
 }
