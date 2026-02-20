7be4f98f134a8e05eb2274e75a7e49c7136168fa
artemyyu <wrestlingteam57@gmail.com>
2026-02-20 14:00:09 +0300

    Split RsiListener into 4 listeners

---
+++ server/src/main/java/ru/screener/listener/bybit/Bybit1DListener.java
@@ -0,0 +1,43 @@
+package ru.screener.listener.bybit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+import ru.screener.dto.bybit.strategies.RsiDto;
+import ru.screener.listener.AbstractKafkaListener;
+import ru.screener.service.deduplication.CaffeineDeduplicationService;
+import ru.screener.service.strategy.StrategyProcessor;
+
+@Component
+@Slf4j
+public class Bybit1DListener extends AbstractKafkaListener<RsiDto> {
+
+    private final StrategyProcessor<RsiDto> strategyProcessor;
+    private final CaffeineDeduplicationService deduplicationService;
+
+    public Bybit1DListener(StrategyProcessor<RsiDto> strategyProcessor,
+                           CaffeineDeduplicationService deduplicationService,
+                           ObjectMapper objectMapper) {
+        super(objectMapper, RsiDto.class);
+        this.strategyProcessor = strategyProcessor;
+        this.deduplicationService = deduplicationService;
+    }
+
+    @KafkaListener(topics = "${app.kafka.topic.bybit1D.name}",
+            containerFactory = "kafkaListenerContainerFactory")
+    public void listen(ConsumerRecord<String, String> record, Acknowledgment ack){
+        onMessage(record, ack);
+    }
+
+    @Override
+    protected void handle(RsiDto dto) {
+        if (!deduplicationService.firstTime(dto.getCrc32())) {
+            log.info("Duplicate message : {}", dto);
+            return;
+        }
+        strategyProcessor.process(dto);
+    }
+}
